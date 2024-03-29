97bd8dd721a76533ffdf0b44cf5c258f4454f60a
Harry Hoffman <hhoffman@ip-solutions.net>
2024-03-29 20:01:21 +0000

    forgot to actually add the files

--- infrastructure-build/provider.tf
+++ infrastructure-build/provider.tf
@@ -3,6 +3,9 @@ terraform {
     libvirt = {
       source = "dmacvicar/libvirt"
     }
+    aws = {
+      source = "hashicorp/aws"
+    }
   }
 }
 
@@ -11,6 +14,11 @@ provider "libvirt" {
   alias = "kvm-00"
   uri   = "qemu+tls://kvm-00/system"
 }
+
+provider "aws" {
+  region = var.aws_region
+}
+
 
 ##provider "libvirt" {
 # Configuration options
--- infrastructure-build/variables.tf
+++ infrastructure-build/variables.tf
@@ -26,6 +26,41 @@ variable "CPU" {
   default = 4
 }
 
+### # ---------------------------------------------------------------------------------------------------------------------
+### # AWS PROVIDER VARIABLES
+### # These are used to connect to AWS
+### # ---------------------------------------------------------------------------------------------------------------------
+### 
+variable "aws_region" {
+  description = "The AWS region to deploy into"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "vpc_cidr" {
+  description = "The CIDR block for the VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "public_subnet_cidr" {
+  description = "The CIDR block for the public subnet"
+  type        = string
+  default     = "10.0.1.0/24"
+}
+
+variable "private_subnet_cidr" {
+  description = "The CIDR block for the private subnet"
+  type        = string
+  default     = "10.0.2.0/24"
+}
+
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+  default     = "t2.micro"
+}
+
 
 ### # ---------------------------------------------------------------------------------------------------------------------
 ### # VMWARE PROVIDER VARIABLES
